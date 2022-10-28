894260b3ba20eb5156bbee94d2a6159e1811ed34
jeremiahk00 <98903687+jeremiahk00@users.noreply.github.com>
2022-10-28 19:32:42 -0400

    Update Student.h

--- midterm_project/Student.h
+++ midterm_project/Student.h
@@ -5,21 +5,21 @@
 using namespace std;
 class Student {
 private:
-	int num_of_books;
 	string username, password;
-	int max_copies;
-	
+	Book copy_list[];
 public:
 	Student();
-	Student(string username, string password, vector <Book> copy_list);
+	Student(string username, string password, Book copy_list[]);
 	string getUser();
 	void setUser(const string username);
 	string getPass();
 	void setPass(const string password);
-	bool borrowBook(string isbn);
-	bool returnBook(string isbn);
 	void printCopies();
 	void insertCopy(const Book b1);
 	void deleteCopy(const Book b1);
+	ostream& operator <<(ostream& output, const Book* book);
+	istream& operator >>(istream& input, Book* book);
+	void UI();
+	void searchBook();
 	void printListOfBooks();
 };
