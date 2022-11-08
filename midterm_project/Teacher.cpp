0f15c1d37e06eb84469e9e54537f46e10f732be0
farhanarahm <42279036+farhanarahm@users.noreply.github.com>
2022-11-07 23:10:43 -0500

    Update Teacher.cpp

--- midterm_project/Teacher.cpp
+++ midterm_project/Teacher.cpp
@@ -5,15 +5,23 @@
 //  Created by Farhana Rahman on 10/22/22.
 //
 
+#include <iostream>
+#include "Student.h"
+#include "Book.h"
+#include <
 #include "teacher.h"
+#include <fstream>
 #include <array>
 #include <string>
+vector <Book> Copy_list;
+vector <Student> students;
+vector <Teacher> teachers;
 using namespace std;
 Teacher::Teacher() {
 
-    username = "Dr.Giambrone";
-    password = "stonybrook2024";
-    max_copies = 0;
+    username = "";
+    password = "";
+    Book copy_list[5];
 }
 
 Teacher::Teacher(string name, string password) {
@@ -67,6 +75,91 @@ void Teacher::set_copies(int max_copies) {
 
     this->max_copies = max_copies;
 }
+
+bool Student::borrow_book(string isbn) {
+    for (int i = 0; i < list_of_books.size() - 1; i++)
+    {
+        if (isbn == list_of_books[i].getISBN())
+        {
+            if (list_of_books[i].getReader() == "")
+			{
+				Copy_list.push_back(list_of_books[i]);
+				list_of_books[i].setReader(username);
+                
+                return 1;
+            }
+            return 0;
+        }
+    }
+    
+    return 0;   
+    
+}
+
+bool Teacher::returnBook(string isbn) {
+	for (int i = 0; i < list_of_books.size() - 1; i++)
+	{
+		if (isbn == list_of_books[i].getISBN()) {
+			list_of_books[i].setReader("");
+		}
+	}
+	for (int j = 0; j < Copy_list.size(); j++)
+	{
+		if (isbn == Copy_list[j].getISBN()) {
+			Copy_list.erase(Copy_list.begin() + j);
+			return 1;
+		}
+	}
+	return 0;
+}
+
+void UI() {
+	char cmd='q';
+	cout << "--------------------------------------------------------------------"<< endl;
+	cout << "-						Welcome to My Library						-"<< endl;
+	cout << "--------------------------------------------------------------------" << endl;
+	cout << "\n";
+	cout << "Welcome back, Teacher";
+	cout << "\n";
+	while (cmd != '0') {
+		cout << "Please choose:" << endl;
+		cout << "\t 1 -- Search Book" << endl;
+		cout << "\t 2 -- Borrow Book" << endl;
+		cout << "\t 3 -- Return Book" << endl;
+		cout << "\t 4 -- Renew Book" << endl;
+        cout << "\t 5 -- Request a new book copy << endl;
+        cout << "\t 6 -- Delete an existing copu << endl;
+		/*add more options here*/
+		cout << "\t 0 -- Log Out" << endl;
+		cin >> cmd;
+		switch (cmd) {
+		case '1':
+			/*search book func*/
+			break;
+		case '2':
+			/*borrow book func*/
+			break;
+		case '3':
+			/*return book func*/
+			break;
+		case '4':
+			/*renew book*/
+			break;
+        case '5':   
+            /*Request book*/    
+            break;
+        case '6':   
+            /*Request book*/    
+            break;
+		default:
+			cout << "Invalid Command. Try Again!" << endl;
+			continue;
+		}
+	}
+	cout << "Logging Out..." << endl;
+	exit(1);
+}
+
 
 
 //Any book they check out goes into this array
